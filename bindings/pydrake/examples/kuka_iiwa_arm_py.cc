c90e1ca81252cd50437f5a93a4d7331f11899d02
Patrick Varin <patrickjvarin@gmail.com>
2018-08-09 14:37:06 -0400

    indentation

--- bindings/pydrake/examples/kuka_iiwa_arm_py.cc
+++ bindings/pydrake/examples/kuka_iiwa_arm_py.cc
@@ -9,25 +9,25 @@ namespace py = pybind11;
 namespace drake {
 namespace pydrake {
 
-  using namespace drake::examples::kuka_iiwa_arm;
-  using drake::systems::LeafSystem;
-
   PYBIND11_MODULE(kuka_iiwa_arm, m) {
-  m.doc() = "Binding lcm system classes for the Kuka Iiwa arm";
+    using namespace drake::examples::kuka_iiwa_arm;
+    using namespace drake::systems;
 
-  py::class_<IiwaCommandReceiver, LeafSystem<double>>(m, "IiwaCommandReceiver")
-    .def(py::init<int>(), py::arg("num_joints")=kIiwaArmNumJoints)
-    .def("set_initial_position", &IiwaCommandReceiver::set_initial_position,
-         py::arg("context"), py::arg("x"));
+    m.doc() = "Binding lcm system classes for the Kuka Iiwa arm";
 
-  py::class_<IiwaCommandSender, LeafSystem<double>>(m, "IiwaCommandSender")
-    .def(py::init<int>(), py::arg("num_joints")=kIiwaArmNumJoints);
+    py::class_<IiwaCommandReceiver, LeafSystem<double>>(m, "IiwaCommandReceiver")
+      .def(py::init<int>(), py::arg("num_joints")=kIiwaArmNumJoints)
+      .def("set_initial_position", &IiwaCommandReceiver::set_initial_position,
+           py::arg("context"), py::arg("x"));
 
-  py::class_<IiwaStatusReceiver, LeafSystem<double>>(m, "IiwaStatusReceiver")
-    .def(py::init<int>(), py::arg("num_joints")=kIiwaArmNumJoints);
+    py::class_<IiwaCommandSender, LeafSystem<double>>(m, "IiwaCommandSender")
+      .def(py::init<int>(), py::arg("num_joints")=kIiwaArmNumJoints);
 
-  py::class_<IiwaStatusSender, LeafSystem<double>>(m, "IiwaStatusSender")
-    .def(py::init<int>(), py::arg("num_joints")=kIiwaArmNumJoints);
+    py::class_<IiwaStatusReceiver, LeafSystem<double>>(m, "IiwaStatusReceiver")
+      .def(py::init<int>(), py::arg("num_joints")=kIiwaArmNumJoints);
+
+    py::class_<IiwaStatusSender, LeafSystem<double>>(m, "IiwaStatusSender")
+      .def(py::init<int>(), py::arg("num_joints")=kIiwaArmNumJoints);
   }
 
 } // namespace pydrake

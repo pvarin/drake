6e32f236e696004bdfe57f5ad169a2cc8227bac8
Patrick Varin <patrickjvarin@gmail.com>
2018-09-05 20:22:30 -0400

    adding QuadraticReeward bindings

--- bindings/pydrake/systems/learning_py.cc
+++ bindings/pydrake/systems/learning_py.cc
@@ -3,6 +3,7 @@
 
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/systems/learning/mdp_diagram.h"
+#include "drake/systems/learning/quadratic_reward.h"
 
 namespace drake {
 namespace pydrake {
@@ -11,7 +12,7 @@ PYBIND11_MODULE(learning, m) {
   // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
   using namespace drake::systems::learning;
 
-  py::class_<MdpDiagram<double>>(m, "MdpDiagram")
+  py::class_<MdpDiagram<double>, systems::Diagram<double>>(m, "MdpDiagram")
       .def(py::init<>())
       .def("Build", &MdpDiagram<double>::Build)
       .def("AddPlant", &MdpDiagram<double>::AddPlant)
@@ -26,6 +27,21 @@ PYBIND11_MODULE(learning, m) {
            &MdpDiagram<double>::get_output_port_observation,
            py::return_value_policy::reference_internal)
       .def("get_output_port_state", &MdpDiagram<double>::get_output_port_state,
+           py::return_value_policy::reference_internal);
+
+  py::class_<QuadraticReward<double>, systems::LeafSystem<double>>(
+      m, "QuadraticReward")
+      .def(py::init<Eigen::MatrixXd, Eigen::VectorXd, Eigen::MatrixXd,
+                    Eigen::VectorXd>(),
+           py::arg("Q"), py::arg("l_x"), py::arg("R"), py::arg("l_u"))
+      .def("get_input_port_action",
+           &QuadraticReward<double>::get_input_port_action,
+           py::return_value_policy::reference_internal)
+      .def("get_input_port_state",
+           &QuadraticReward<double>::get_input_port_state,
+           py::return_value_policy::reference_internal)
+      .def("get_output_port_reward",
+           &QuadraticReward<double>::get_output_port_reward,
            py::return_value_policy::reference_internal);
 }
 
